9d2deca42b96c147c12a2bcf55913bec3c991c58
lanmai1024 <zhen080702@gmail.com>
2024-11-27 01:29:54 +0800

    修改缓存资源路径

--- service_worker.js
+++ service_worker.js
@@ -1,19 +1,19 @@
 const CACHE_NAME = "Service Worker 0.0.5";
 const ASSETS = [
-    "/",
-    "/index.html",
-    "/index_style.css",
-    "/js/weather.js",
-    "/img/fog.png",
-    "/img/heavy_rain.png",
-    "/img/lightning.png",
-    "/img/rainy.png",
-    "/img/showers.png",
-    "/img/sun.png",
-    "/img/thunderstorm.png",
-    "/img/warning.png",
-    "/img/icons/icon-192x192.png",
-    "/img/icons/icon-512x512.png"
+    "/Summer-s-eye-website/",
+    "/Summer-s-eye-website/index.html",
+    "/Summer-s-eye-website/index_style.css",
+    "/Summer-s-eye-website/js/weather.js",
+    "/Summer-s-eye-website/img/fog.png",
+    "/Summer-s-eye-website/img/heavy_rain.png",
+    "/Summer-s-eye-website/img/lightning.png",
+    "/Summer-s-eye-website/img/rainy.png",
+    "/Summer-s-eye-website/img/showers.png",
+    "/Summer-s-eye-website/img/sun.png",
+    "/Summer-s-eye-website/img/thunderstorm.png",
+    "/Summer-s-eye-website/img/warning.png",
+    "/Summer-s-eye-website/img/icons/icon-192x192.png",
+    "/Summer-s-eye-website/img/icons/icon-512x512.png"
 ];
 // 安裝事件：緩存所需資源
 self.addEventListener("install", (event) => {
